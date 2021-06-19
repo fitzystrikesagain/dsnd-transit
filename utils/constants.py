73f006c122197bd6867b27eeaae6c8f3c1bcc961
Fitzy <fitzystrikesagain@gmail.com>
2021-06-19 16:26:00 -0500

    move creds to .env and pg config to constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,11 @@
+import os
+
 BROKER_URL = "PLAINTEXT://localhost:9092"
 SCHEMA_REGISTRY_URL = "http://localhost:8081"
+KAFKA_CONNECT_URL = "http://localhost:8083/connectors"
+
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+JDBC_URL = f"jdbc:postgresql://{POSTGRES_HOST}:5432/{POSTGRES_DB}"
